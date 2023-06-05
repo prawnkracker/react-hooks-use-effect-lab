7de446024013dfeca64de7fd388c4161cc100ab0
Dennis Demir <dennisjdemir@gmail.com>
2023-06-04 22:35:18 -0400

    complete lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000)
+    if (timeRemaining === 0){
+      setTimeRemaining(10);
+      onAnswered(false)
+    }
+    console.log(timeRemaining)
+
+    return function cleanup(){
+      clearTimeout(timerId)
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
